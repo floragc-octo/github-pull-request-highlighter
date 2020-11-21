06feb4414bafe0bef23d7c9615d88fa0ed2cf2b8
FloraGC <fguycoichard@octo.com>
2020-11-22 00:15:48 +0100

    feature - add support for customization page with chrome storage - WIP

--- coloration.js
+++ coloration.js
@@ -11,7 +11,7 @@ const current_date = new Date()
 // QUERIES
 
 const pr_list = () => document.querySelectorAll(PR_SELECTOR)
-const date_diff = (pr_date, date=current_date) => date - pr_date
+const date_diff = (pr_date, date = current_date) => date - pr_date
 const get_date = (pr) => new Date(pr.querySelector('relative-time').getAttribute('datetime'))
 
 const is_draft = (pr) => pr.querySelector(DRAFT_PR_SELECTOR)
@@ -19,15 +19,17 @@ const is_approved = (pr) => pr.querySelector(APPROVED_PR_SELECTOR)
 const is_old = (pr) => date_diff(get_date(pr)) > pr_obsolence
 const set_color = (pr, color) => pr.style.backgroundColor = color
 
-const init = () =>  {
-    pr_list().forEach((pr) => {
-        if (is_draft(pr)) set_color(pr, "#C2CAD0")
-        if (is_approved(pr)) set_color(pr, "#379683")
-        if (is_old(pr)) set_color(pr, "#E7717D")
-    })
+const init = () => {
+    chrome.storage.sync.get({draft_color: "#C2CAD0"}, function (items) {
+        pr_list().forEach((pr) => {
+            if (is_draft(pr)) set_color(pr, items.draft_color)
+            if (is_approved(pr)) set_color(pr, "#379683")
+            if (is_old(pr)) set_color(pr, "#E7717D")
+        })
+    });
 }
 
 init()
-document.addEventListener('pjax:end', function(event) {
+document.addEventListener('pjax:end', function (event) {
     init()
 });
---
+++ options.js
@@ -0,0 +1,9 @@
+function toto() {
+    var color = document.getElementById('draft_color').value;
+
+    chrome.storage.sync.set({ draft_color: color }, function () {
+        console.log('Settings saved');
+    });
+}
+
+document.getElementById('customization-form').addEventListener('submit', toto)
