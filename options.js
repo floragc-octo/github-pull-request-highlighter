078e55a1717250ebcc54318f589ea43b59e0f6d4
FloraGC <fguycoichard@octo.com>
2020-11-22 17:01:15 +0100

    feature - add obsolescence delay in settings - WIP saved in storage but not used in front yet because other pr is refactoring this part

--- options.js
+++ options.js
@@ -1,32 +1,35 @@
 const draftDOM = document.getElementById('draft');
 const approvedDOM = document.getElementById('approved');
 const obsoleteDOM = document.getElementById('obsolete');
+const obsolescenceDOM = document.getElementById('obsolescense');
 
-const defaultColors = {
+const defaultCommonSettings = {
     draft_color: "#2A4860",
     approved_color: "#379683",
     obsolete_color: "#E7717D",
+    pr_obsolescence_in_day: "15"
 }
 
-const setColors = ({ draft_color, approved_color, obsolete_color }) => {
+const displayCommonSettings = ({ draft_color, approved_color, obsolete_color, pr_obsolescence_in_day }) => {
     draftDOM.value = draft_color;
     approvedDOM.value = approved_color;
     obsoleteDOM.value = obsolete_color;
+    obsolescenceDOM.value = pr_obsolescence_in_day;
 }
 
-const getColors = () => ({
+const getCommonSettings = () => ({
     draft_color: draftDOM.value,
     approved_color: approvedDOM.value,
     obsolete_color: obsoleteDOM.value,
+    pr_obsolescence_in_day: obsolescenceDOM.value,
 })
 
-const saveSettings = () => chrome.storage.sync.set(getColors());
+const saveSettings = () => chrome.storage.sync.set({...getCommonSettings()});
 
-const retrieveSettings = () => chrome.storage.sync.get(defaultColors,setColors);
+const retrieveSettings = () => chrome.storage.sync.get(defaultCommonSettings, displayCommonSettings);
 
-const resetDefaultColors = () => setColors(defaultColors)
+const resetDefaultCommonSettings = () => displayCommonSettings(defaultCommonSettings)
 
 document.addEventListener('DOMContentLoaded', retrieveSettings);
 document.getElementById('customization-form').addEventListener('submit', saveSettings)
-document.getElementById('form-reset').addEventListener('click', resetDefaultColors)
-
+document.getElementById('form-reset').addEventListener('click', resetDefaultCommonSettings)
