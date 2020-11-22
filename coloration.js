d5d3cb8ed3f37413bc23552720048436fe1fbd1c
FloraGC <fguycoichard@octo.com>
2020-11-22 14:23:17 +0100

    feature - add support for all existing colors in new redesign - localStorage still WIP

--- coloration.js
+++ coloration.js
@@ -1,4 +1,4 @@
-// CONFIG
+// SELECTORS
 
 const PR_SELECTOR = '[aria-label="Issues"] .js-issue-row'
 const DRAFT_PR_SELECTOR = '[aria-label="Open draft pull request"]'
@@ -8,7 +8,6 @@ const PR_OBSOLENCE_DELAY_IN_DAYS = 15
 const pr_obsolence = PR_OBSOLENCE_DELAY_IN_DAYS * 3600 * 24 * 1000
 const current_date = new Date()
 
-// QUERIES
 
 const pr_list = () => document.querySelectorAll(PR_SELECTOR)
 const date_diff = (pr_date, date = current_date) => date - pr_date
@@ -16,24 +15,67 @@ const get_date = (pr) => new Date(pr.querySelector('relative-time').getAttribute
 
 const is_draft = (pr) => pr.querySelector(DRAFT_PR_SELECTOR)
 const is_approved = (pr) => pr.querySelector(APPROVED_PR_SELECTOR)
-const is_old = (pr) => date_diff(get_date(pr)) > pr_obsolence
+const is_obsolete = (pr) => date_diff(get_date(pr)) > pr_obsolence
 
-const set_color = (pr, color) => {
-    pr.style.borderLeft = "15px solid "+color;
-    pr.style.borderRight = "8px solid "+color;
-    pr.style.borderTop = "0";
-    pr.style.backgroundColor = color+"20";
+
+// CSS CONFIG
+
+chrome.storage.sync.get({
+    draft_color: "#C2CAD0",
+    approved_color: "#379683",
+    obsolete_color: "#E7717D",
+    default_color: "transparent"
+}, function ({
+    draft_color, 
+    approved_color, 
+    obsolete_color, 
+    default_color
+}) {
+    const CSS = `
+        [aria-label="Issues"] .js-issue-row {
+            border-left: 15px solid ${default_color};
+            border-right: 8px solid ${default_color};
+            border-top: 0;
+        }
+        [aria-label="Issues"] .js-issue-row.draft {
+            border-color: ${draft_color};
+            background-color: ${draft_color}20;
+        }
+        [aria-label="Issues"] .js-issue-row.draft:hover {
+            background-color: ${draft_color}40;
+        }
+        [aria-label="Issues"] .js-issue-row.approved {
+            border-color: ${approved_color};
+            background-color: ${approved_color}20;
+        }
+        [aria-label="Issues"] .js-issue-row.approved:hover {
+            background-color: ${approved_color}40;
+        }
+        [aria-label="Issues"] .js-issue-row.obsolete {
+            border-color: ${obsolete_color};
+            background-color: ${obsolete_color}20;
+        }
+        [aria-label="Issues"] .js-issue-row.obsolete:hover {
+            background-color: ${obsolete_color}40;
+        }
+    `
+    const style = document.createElement("style")
+    style.appendChild(document.createTextNode(CSS))
+    document.getElementsByTagName("head")[0].appendChild(style)
+})
+
+const set_color = (pr, status) => {
+    pr.classList.add(status)
 }
 
+// QUERIES
+
 const init = () => {
-    chrome.storage.sync.get({draft_color: "#C2CAD0"}, function (items) {
-        pr_list().forEach((pr) => {
-            if (is_draft(pr)) set_color(pr, items.draft_color)
-            if (is_approved(pr)) set_color(pr, "#379683")
-            if (is_old(pr)) set_color(pr, "#E7717D")
-            if (!pr.style.borderLeft) set_color(pr, "transparent")
-        })
-    });
+    pr_list().forEach((pr) => {
+        if (is_draft(pr)) set_color(pr, 'draft')
+        if (is_approved(pr)) set_color(pr, 'approved')
+        if (is_obsolete(pr)) set_color(pr, 'obsolete')
+    })
 }
 
 init()
--- options.js
+++ options.js
@@ -2,7 +2,7 @@ function toto() {
     const color = document.getElementById('draft_color').value;
 
     chrome.storage.sync.set({ draft_color: color }, function () {
-        console.log('Settings saved');
+        console.log('Settings saved'); // todo better info to user
     });
 }
 
