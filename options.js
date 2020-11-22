9b76a18f56b530b471a3fa87219a4242610ce2fe
FloraGC <fguycoichard@octo.com>
2020-11-22 14:51:31 +0100

    feature - add localStorage saving and restoring for all customisable colors

--- options.js
+++ options.js
@@ -1,9 +1,30 @@
-function toto() {
-    const color = document.getElementById('draft_color').value;
+function saveSettings() {
+    const draft = document.getElementById('draft').value;
+    const approved = document.getElementById('approved').value;
+    const obsolete = document.getElementById('obsolete').value;
 
-    chrome.storage.sync.set({ draft_color: color }, function () {
-        console.log('Settings saved'); // todo better info to user
+    chrome.storage.sync.set({
+        draft_color: draft,
+        approved_color: approved,
+        obsolete_color: obsolete,
+    }, function () {
+        console.log('Settings saved.')
+        var status = document.getElementById('status');
+        status.textContent = 'Settings saved.';
     });
 }
 
-document.getElementById('customization-form').addEventListener('submit', toto)
+function retrieveSettings() {
+    chrome.storage.sync.get({
+        draft_color: "#C2CAD0",
+        approved_color: "#379683",
+        obsolete_color: "#E7717D",
+    }, function (items) {
+        document.getElementById('draft').value = items.draft_color;
+        document.getElementById('approved').value = items.approved_color;
+        document.getElementById('obsolete').value = items.obsolete_color;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', retrieveSettings);
+document.getElementById('customization-form').addEventListener('submit', saveSettings)
