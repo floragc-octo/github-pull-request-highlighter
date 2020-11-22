48e307bd5bb95b69e54bcda503fb161db4c43c1e
FloraGC <fguycoichard@octo.com>
2020-11-22 20:06:21 +0100

    feature - add support for review required status

--- extension/src/settings/user_settings.js
+++ extension/src/settings/user_settings.js
@@ -2,6 +2,7 @@ const draftDOM = document.getElementById('draft');
 const approvedDOM = document.getElementById('approved');
 const obsoleteDOM = document.getElementById('obsolete');
 const requestChangesDOM = document.getElementById('request-changes');
+const reviewRequiredDOM = document.getElementById('review-required');
 const obsolescenceDOM = document.getElementById('obsolescense');
 
 const defaultCommonSettings = {
@@ -9,14 +10,19 @@ const defaultCommonSettings = {
     approved_color: DEFAULT_APPROVED_COLOR,
     obsolete_color: DEFAULT_OBSOLETE_COLOR,
     request_changes_color: DEFAULT_REQUEST_CHANGES_COLOR,
+    review_required_color: DEFAULT_REVIEW_REQUIRED_COLOR,
     pr_obsolescence_in_day: DEFAULT_OBSOLESCENCE_IN_DAY,
 }
 
-const displayCommonSettings = ({ draft_color, approved_color, obsolete_color, request_changes_color, pr_obsolescence_in_day }) => {
+const displayCommonSettings = ( settings ) => {
+    const {
+        draft_color, approved_color, obsolete_color, request_changes_color, review_required_color, pr_obsolescence_in_day
+    } = settings
     draftDOM.value = draft_color;
     approvedDOM.value = approved_color;
     obsoleteDOM.value = obsolete_color;
     requestChangesDOM.value = request_changes_color;
+    reviewRequiredDOM.value = review_required_color;
     obsolescenceDOM.value = pr_obsolescence_in_day;
 }
 
@@ -25,6 +31,7 @@ const getCommonSettings = () => ({
     approved_color: approvedDOM.value,
     obsolete_color: obsoleteDOM.value,
     request_changes_color: requestChangesDOM.value,
+    review_required_color: reviewRequiredDOM.value,
     pr_obsolescence_in_day: obsolescenceDOM.value,
 })
 
