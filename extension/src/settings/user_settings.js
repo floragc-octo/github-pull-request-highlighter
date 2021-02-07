e22390eb638561fd233414410db35dcc24c38403
Fabien Mercier <fmercier@octo.com>
2021-02-07 11:29:35 +0100

    Tech - Linting

--- extension/src/settings/user_settings.js
+++ extension/src/settings/user_settings.js
@@ -3,55 +3,68 @@ const approvedDOM = document.getElementById('approved')
 const obsoleteDOM = document.getElementById('obsolete')
 const requestChangesDOM = document.getElementById('request-changes')
 const reviewRequiredDOM = document.getElementById('review-required')
-const obsolescenceDOM = document.getElementById('obsolescense')
+const obsolescenceDOM = document.getElementById('obsolescence')
 
 const defaultCommonSettings = {
-    draft_color: DEFAULT_DRAFT_COLOR,
-    approved_color: DEFAULT_APPROVED_COLOR,
-    obsolete_color: DEFAULT_OBSOLETE_COLOR,
-    request_changes_color: DEFAULT_REQUEST_CHANGES_COLOR,
-    review_required_color: DEFAULT_REVIEW_REQUIRED_COLOR,
-    pr_obsolescence_in_day: DEFAULT_OBSOLESCENCE_IN_DAY,
+  draftColor: DEFAULT_DRAFT_COLOR,
+  approvedColor: DEFAULT_APPROVED_COLOR,
+  obsoleteColor: DEFAULT_OBSOLETE_COLOR,
+  requestChangesColor: DEFAULT_REQUEST_CHANGES_COLOR,
+  reviewRequiredColor: DEFAULT_REVIEW_REQUIRED_COLOR,
+  prObsolescenceInDay: DEFAULT_OBSOLESCENCE_IN_DAY,
 }
 
-const displayCommonSettings = (settings) => {
-    const {
-        draft_color, approved_color, obsolete_color, request_changes_color, review_required_color, pr_obsolescence_in_day
-    } = settings
-    draftDOM.value = draft_color
-    approvedDOM.value = approved_color
-    obsoleteDOM.value = obsolete_color
-    requestChangesDOM.value = request_changes_color
-    reviewRequiredDOM.value = review_required_color
-    obsolescenceDOM.value = pr_obsolescence_in_day
-
-    const color_list = document.getElementById('color-list')
-    color_list.innerHTML = ''
-    const color_values = Object.values([...Object.values(defaultCommonSettings), draft_color, approved_color, obsolete_color, request_changes_color, review_required_color])
-    color_values
-        .filter(isColor)
-        .map(toUppercase)
-        .filter(isUnique)
-        .forEach((color) => {
-            const option = document.createElement('option')
-            option.value = color
-            color_list.appendChild(option)
-        })
-}
-
-const getCommonSettings = () => ({
-    draft_color: draftDOM.value,
-    approved_color: approvedDOM.value,
-    obsolete_color: obsoleteDOM.value,
-    request_changes_color: requestChangesDOM.value,
-    review_required_color: reviewRequiredDOM.value,
-    pr_obsolescence_in_day: obsolescenceDOM.value,
-})
-
-const saveSettings = () => store.set({...getCommonSettings()})
 const isColor = (value) => value && typeof value === 'string' && value.indexOf('#') === 0
 const toUppercase = (value) => value.toUpperCase()
 const isUnique = (value, index, self) => self.indexOf(value) === index
+
+const displayCommonSettings = (settings) => {
+  const {
+    draftColor,
+    approvedColor,
+    obsoleteColor,
+    requestChangesColor,
+    reviewRequiredColor,
+    prObsolescenceInDay,
+  } = settings
+  draftDOM.value = draftColor
+  approvedDOM.value = approvedColor
+  obsoleteDOM.value = obsoleteColor
+  requestChangesDOM.value = requestChangesColor
+  reviewRequiredDOM.value = reviewRequiredColor
+  obsolescenceDOM.value = prObsolescenceInDay
+
+  const colorList = document.getElementById('color-list')
+  colorList.innerHTML = ''
+  const colorValues = Object.values([
+    ...Object.values(defaultCommonSettings),
+    draftColor,
+    approvedColor,
+    obsoleteColor,
+    requestChangesColor,
+    reviewRequiredColor,
+  ])
+  colorValues
+    .filter(isColor)
+    .map(toUppercase)
+    .filter(isUnique)
+    .forEach((color) => {
+      const option = document.createElement('option')
+      option.value = color
+      colorList.appendChild(option)
+    })
+}
+
+const getCommonSettings = () => ({
+  draftColor: draftDOM.value,
+  approvedColor: approvedDOM.value,
+  obsoleteColor: obsoleteDOM.value,
+  requestChangesColor: requestChangesDOM.value,
+  reviewRequiredColor: reviewRequiredDOM.value,
+  prObsolescenceInDay: obsolescenceDOM.value,
+})
+
+const saveSettings = () => store.set({ ...getCommonSettings() })
 
 const retrieveSettings = () => store.get(defaultCommonSettings, displayCommonSettings)
 
